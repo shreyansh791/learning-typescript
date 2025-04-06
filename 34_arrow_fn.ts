b87814eff8dd47d679a74e6ff5f98e9c917b6bc6
Shreyansh Jain <shreyansh791@yahoo.in>
2025-04-06 23:33:19 +0530

    updated

--- 34_arrow_fn.ts
+++ 34_arrow_fn.ts
@@ -8,7 +8,9 @@ const fn = (num1: number, num2: number) => {
     return num1 + num2
 }
 
-const fn2: (num1: number, num2: number) => number = (num1, num2) => num1 + num2
+const fn2 = (num1: number, num2: number) => num1 + num2
+const fn3: (num1: number, num2: number) => number = (num1, num2) => num1 + num2
 console.log(sum(10, 20))
 console.log(fn(10, 20))
 console.log(fn2(20, 40))
+console.log(fn3(30, 40))
