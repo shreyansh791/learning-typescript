43f5bb925dbe9a768cf0d9a3a4be06e2ad696414
Shreyansh Jain <shreyansh791@yahoo.in>
2025-04-13 17:01:41 +0530

    file modified

--- oops/interface_demo.ts
+++ oops/interface_demo.ts
@@ -2,25 +2,27 @@ export { }
 interface User {
     firstName: string;
     lastName: string;
+    company?: string // optional property
     greetUser(): void;
     getFullName(): string;
 }
 class Admin implements User {
-    constructor(public firstName: string, public lastName: string) {
+    public company?: string | undefined = 'Accenture'; // setting default value
+    constructor(public firstName: string, public lastName: string, company?: string) {
     }
     greetUser(): void {
         console.log("Hello Admin");
     }
     getFullName(): string {
-        return this.firstName + " " + this.lastName
+        return this.firstName + " " + this.lastName +" and company is "+ this.company
     }
 
 }
 
 class Member implements User {
-    constructor(public firstName: string, public lastName: string) {
+    constructor(public firstName: string, public lastName: string, public company?:string) {
     }
-    greetUser(): void {
+     greetUser(): void {
         console.log("Hello Member");
     }
     getFullName(): string {
@@ -28,11 +30,11 @@ class Member implements User {
     }
 
 }
-function displayGreetMessage(user: User) {
+ function displayGreetMessage(user: User) {
     user.greetUser();
     console.log(user.getFullName());
 }
-const admin = new Admin("Shreyansh", "Jain")
+const admin = new Admin("Shreyansh", "Jain" ,"Globant")
 const member = new Member("Akshay", "Jain")
 displayGreetMessage(admin)
 displayGreetMessage(member)
